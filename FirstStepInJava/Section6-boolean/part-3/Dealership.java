a90b2ff0b51203207a7ffa7b0bdf2c8f3387a866
yeun22 <yeun.lefloch@gmail.com>
2022-09-16 08:34:34 +0200

    section 7 terminée

--- FirstStepInJava/Section6-boolean/part-3/Dealership.java
+++ FirstStepInJava/Section6-boolean/part-3/Dealership.java
@@ -13,10 +13,39 @@ public class Dealership {
 
         switch (option) {
             case "a":
-                System.out.println("you choose option " + option);
+                System.out.println("What is your budget ?");
+                int budget = scan.nextInt();
+                if(budget >=10000){
+                    System.out.println("Great ! A Nissan Altima is available");
+                    System.out.println("\n Do you have insurrance ? (yes/no)");
+                    scan.nextLine();
+                    String insurance = scan.nextLine();
+                    System.out.println("\nDo you have a license ? (yes/no)");
+                    String license = scan.nextLine();
+                    System.out.println("\nWhat is your credit score ?");
+                    int creditScore = scan.nextInt();
+                    if(insurance.equals("yes") && license.equals("yes") && creditScore >= 660){
+                        System.out.println("Sold ! Pleasure doing business with you !");
+                    } else {
+                        System.out.println("We're sorry, you're not eligible !");
+                    }
+
+                } else {
+                    System.out.println("Sorry we don't sell car under $10,000 !");
+                }
                 break;
             case "b":
-                System.out.println("you choose option " + option);
+                System.out.println("\nWhat is your car value at ?");
+                int value = scan.nextInt();
+                System.out.println("\nWhat is your selling price ?");
+                int price = scan.nextInt();
+
+                if(value > price && price < 30000){
+                    System.out.println("\nWe will buy your car if you don't lie about value of it");
+                }else {
+                    System.out.println("\nSorry we can't buy your car ! We're not interested !");
+                }
+
                 break;
         
             default: System.out.println("Invalid options !");
